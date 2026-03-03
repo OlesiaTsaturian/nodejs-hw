f939d7bba278f24cd1f4c9a32d9ed1c52c47e0f0
Olesia Tsaturian <olesiases@gmail.com>
2026-03-03 14:23:21 +0200

    Rename DB/ to db/

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { connectMongoDB } from './datab/connectMongoDB.js';
+import { connectMongoDB } from './DB/connectMongoDB.js';
 
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
