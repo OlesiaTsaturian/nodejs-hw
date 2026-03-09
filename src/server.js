6bdddfd1e2c19a994a6831484418ef293a582601
Olesia Tsaturian <olesiases@gmail.com>
2026-03-09 16:59:00 +0200

    add validation for post, patch, delete and get

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
 import notesRouter from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -18,6 +19,8 @@ app.use(cors());
 app.use(notesRouter);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
---
+++ src/validations/notesValidation.js
@@ -0,0 +1,44 @@
+import { Joi, Segments } from 'celebrate';
+import { TAGS } from '../constants/tags.js';
+import { isValidObjectId } from 'mongoose';
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).max(50).trim().required().messages({
+      'string.base': 'Title should be a string',
+      'string.min': 'Title should have at least {#limit} characters',
+      'string.max': 'Title should have at most {#limit} characters',
+      'any.required': 'Title is required',
+    }),
+
+    content: Joi.string()
+      .trim()
+      .default('')
+      .messages({ 'string.base': 'Content should be a string' }),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .default('Todo')
+      .messages({ 'any.only': `Tag must be one of ${TAGS}` }),
+  }),
+};
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid Id format') : value;
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).trim(),
+    content: Joi.string().trim(),
+    tag: Joi.string().valid(...TAGS),
+  }).min(1),
+};
