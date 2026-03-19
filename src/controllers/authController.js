5daa9d8b2197e638f55588daf67cc7f517d446e5
Olesia Tsaturian <olesiases@gmail.com>
2026-03-19 13:26:21 +0200

    add registration and login

---
+++ src/controllers/authController.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import { request, response } from 'express';
+
+export const registerUser = async (request, response) => {
+  const { email, password } = request.body;
+
+  const existUser = await User.findOne({ email });
+  if (existUser) {
+    throw createHttpError(400, 'Email in use');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  response.status(201).json(newUser);
+};
+
+export const loginUser = async (request, response) => {
+  const { email, password } = request.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+  response.status(200).json(user);
+};
--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
-
+import { errors } from 'celebrate';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
-import notesRouter from './routes/notesRoutes.js';
-import { errors } from 'celebrate';
+import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -16,7 +16,8 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 
-app.use(notesRouter);
+app.use(authRoutes);
+app.use(notesRoutes);
 
 app.use(notFoundHandler);
 
